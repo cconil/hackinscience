61357f5cab592e197e8292c8226811264f456898
ClementCblondie <clement.conil@cri-paris.org>
2015-12-04 15:53:22 +0100

    exo85

--- exercises/085/solution.py
+++ exercises/085/solution.py
@@ -1,3 +1,11 @@
 def sort_a_list(list):
-    for i in range(len(list)):
-        return(sorted(list, reverse=True))
+    return(sorted(list, reverse=True))
+
+
+def sort_by_mark(list):
+    return(sorted(list, reverse=True))
+
+
+from operator import itemgetter
+def sort_by_name(list):
+    return(sorted(list, key=itemgetter(1)))
