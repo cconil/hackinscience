86971edd3cf4cbdbcc9885715c49b74c0276c546
ClementCblondie <clement.conil@cri-paris.org>
2015-12-15 10:37:05 +0100

    exo97version2

--- exercises/097/solution.py
+++ exercises/097/solution.py
@@ -1,9 +1,9 @@
 def love_meet(bob, alice):
-    for i in set(alice)&set(bob):
+    for i in set(alice) & set(bob):
         print(i)
 
 
 def affair_meet(bob, alice, silvester):
-    for i in set(alice)&set(silvester):
+    for i in set(alice) & set(silvester):
         if i not in set(bob):
             print(i)
